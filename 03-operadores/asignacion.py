e0e816eb059f603f86c002653259c07e3c1c10db
Tony Suarez <alexcanquis3109@gmail.com>
2022-06-05 21:03:17 -0400

    Voy en los condicionales en python

---
+++ 03-operadores/asignacion.py
@@ -0,0 +1,29 @@
+# Operadores incremento y decremento
+#Incremento
+year = 2021
+
+#year = year + 1
+year += 1
+
+
+#decremento
+#year = year - 1
+year -= 1
+
+# Pre incremento
+year = 1 + year
+
+# Pre decremento
+year = 1 - year
+
+
+print (year)
+
+
+
+"""
+year++
+year-- <---- estos no existen en python
+--year
+++year
+"""
---
+++ 04-entrada-salida/main.py
@@ -0,0 +1,7 @@
+#Entrada
+nombre = input("¿Cual es tu nombre?: ")
+edad = input("Cual es tu edad?: ")
+
+#salida
+print(f"Me alegro por conocerte {nombre}, veo que tienes {edad} años")
+
